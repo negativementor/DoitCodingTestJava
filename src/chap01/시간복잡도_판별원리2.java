5e271d0abd976ffe7677103fbcd77cb43a7d275b
negativementor <negativementor@gmail.com>
2025-09-23 17:58:44 +0900

    2025.09.23 chap02

---
+++ src/chap01/시간복잡도_판별원리2.java
@@ -0,0 +1,19 @@
+package chap01;
+
+public class 시간복잡도_판별원리2 {
+
+	public static void main(String[] args) {
+		int N = 100000;
+		int cnt = 0;
+		for(int i=0; i < N; i++) {
+			System.out.println("연산 횟수:" + cnt++);
+		}
+		for(int i=0; i < N; i++) {
+			System.out.println("연산 횟수:" + cnt++);
+		}
+		for(int i=0; i < N; i++) {
+			System.out.println("연산 횟수:" + cnt++);
+		}
+	}
+
+}
