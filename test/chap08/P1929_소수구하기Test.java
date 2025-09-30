19355e7ce6d49cb201c391344c57c4dc2bdedcfb
negativementor <negativementor@gmail.com>
2025-09-30 17:57:02 +0900

    2025.09.30 chap08 P.257

---
+++ test/chap08/P1929_소수구하기Test.java
@@ -0,0 +1,27 @@
+package chap08;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.jupiter.api.Test;
+
+class P1929_소수구하기Test {
+
+	@Test
+	void testMain() {
+
+		//예제1
+		String sInput1 = "3 16\n";
+		exInput(sInput1);
+		P1929_소수구하기.main(null);
+		
+	}
+
+	void exInput(String input) {
+		System.out.println("\n===입력===");
+		System.out.println(input);
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        System.out.println("===출력===");
+	}
+
+}
